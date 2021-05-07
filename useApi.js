f42701bb997c5c9ced6b2312e20242d400ccaf1d
Saransh <saransh0701@gmail.com>
2021-05-07 23:25:07 +0530

    Update example

--- useApi.js
+++ useApi.js
@@ -1,7 +1,7 @@
 const axios = require('./axios');
 
 async function fetchData() {
-    const req = await axios.get('/100');
+    const req = await axios.get('/memes/10');
 
     console.log(req.data);
 }
